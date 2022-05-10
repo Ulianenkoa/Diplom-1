9f6469a1a35b715cd3365d38357bb4b07cc474f5
Anastasiya Ulyanenko <ulianenkoa@yandex.ru>
2022-05-10 15:32:49 +0300

    Add BurgerTest/IngredientTypeTest/BurgerParameterizedTest, change BunTest.

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -1,20 +1,39 @@
 package praktikum;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class BunTest {
+    private final Bun bun;
+    private final String expectedNameBun;
+    private final float expectedPriceBun;
 
+
+    public BunTest(Bun bun, String expectedNameBun, float expectedPriceBun) {
+        this.bun = bun;
+        this.expectedNameBun = expectedNameBun;
+        this.expectedPriceBun = expectedPriceBun;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getBunData() {
+        return new Object[][]{
+                {new Bun("red bun", 300), "red bun", 300},
+                {new Bun("white bun", 200), "white bun", 200},
+                {new Bun("red bun", 300), "red bun", 300},
+        };
+    }
     @Test
     public void getNameBunShouldBeCorrect(){
-        Bun bun = new Bun("red bun", 300);
-        assertEquals("Некорректное наименование булочки", "red bun", bun.getName());
+        assertEquals("Некорректное наименование булочки",expectedNameBun,bun.getName());
     }
 
     @Test
     public void getPriceBunShouldBeCorrect(){
-        Bun bun = new Bun("red bun", 300);
-        assertEquals( 300, bun.getPrice(),0);
+        assertEquals(expectedPriceBun,bun.getPrice(),0);
     }
 }
---
+++ src/test/java/praktikum/IngredientTypeTest.java
@@ -0,0 +1,31 @@
+package praktikum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class IngredientTypeTest {
+    private final IngredientType type;
+    private final String expected;
+
+    public IngredientTypeTest(IngredientType type, String expected) {
+        this.type = type;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getIngredientTypeData(){
+        return new Object[][]{
+                {IngredientType.FILLING,"FILLING"},
+                {IngredientType.SAUCE,"SAUCE"},
+        };
+        }
+
+    @Test
+    public void checkIngredientTypeTest(){
+        assertEquals(type.name(), expected);
+    }
+}
